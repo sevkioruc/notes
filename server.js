f58de4b1a8bd5911fdf9b0904d0fa370c9553750
sevkioruc <sevkioruc34@gmail.com>
2021-03-08 01:22:10 +0300

    Mongo config added

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ dotenv.config();
 
 const app = express();
 
+mongoose.connect(process.env.mongoURI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+})
+	.then(() => {
+		console.log('Connected to DB');
+	})
+	.catch(() => {
+		console.log('Connection failed');
+	});
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,5 +26,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const port = process.env.PORT || 5000;
 
 app.listen(port, (err) => {
-	console.log(err);
+	if (err)
+		console.log(err);
 });
