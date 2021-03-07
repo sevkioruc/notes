140d765a1a023471929fd2d7feac1eed867c1a6f
sevkioruc <sevkioruc34@gmail.com>
2021-03-07 21:35:26 +0300

    Register form added

---
+++ client/src/components/Register.vue
@@ -0,0 +1,65 @@
+<template>
+  <b-container fluid class="container">
+    <b-form class="register-form" @submit="onSubmit">
+      <b-form-group class="mb-3">
+        <b-form-input
+          v-model="form.email"
+          type="email"
+          placeholder="Email"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group class="mb-3">
+        <b-form-input
+          v-model="form.username"
+          placeholder="Username"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group>
+        <b-form-input
+          v-model="form.password"
+          type="password"
+          placeholder="Password"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <div class="d-flex">
+        <b-button class="ml-auto" type="submit" variant="primary">
+          Register
+        </b-button>
+      </div>
+    </b-form>
+  </b-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        email: "",
+        username: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    onSubmit(event) {
+      event.preventDefault();
+    },
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  text-align: center;
+}
+.register-form {
+  margin-top: 100px;
+}
+</style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Register from '../components/Register';
 
 Vue.use(Router)
 
 export default new Router({
+	mode: 'history',
 	routes: [
+		{
+			path: '/register',
+			component: Register
+		}
 	]
 })
