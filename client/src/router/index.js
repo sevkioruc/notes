807a8d3aa31d172185060a48fc9287aaf9c131c6
sevkioruc <sevkioruc34@gmail.com>
2021-03-08 21:50:04 +0300

    Homepage component created

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Register from '../components/Register';
+import Homepage from '../components/Homepage';
 
 Vue.use(Router)
 
 export default new Router({
 	mode: 'history',
 	routes: [
+		{
+			path: '/',
+			component: Homepage
+		},
 		{
 			path: '/register',
 			component: Register
