72a6b256f71ea7f410ad28d492b7007a931af409
sevkioruc <sevkioruc34@gmail.com>
2021-03-10 23:06:38 +0300

    Navbar added

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-nav>
+      <b-nav-item href="#">Homepage</b-nav-item>
+    </b-navbar-nav>
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav class="ml-auto">
+        <b-navbar-nav>
+          <b-nav-item to="/login">Login</b-nav-item>
+          <b-nav-item to="/register">Register</b-nav-item>
+          <b-nav-item>Logout</b-nav-item>
+        </b-navbar-nav>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
