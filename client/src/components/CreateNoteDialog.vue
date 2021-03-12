22099b893f6e5176e66bde4e42915f4e1eef6dfd
sevkioruc <sevkioruc34@gmail.com>
2021-03-13 02:49:01 +0300

    Create note component added

---
+++ client/src/components/CreateNoteDialog.vue
@@ -0,0 +1,34 @@
+<template>
+  <b-modal id="createNoteModal" size="lg" centered @ok="saveNote">
+    <template #modal-footer="{ ok }">
+      <b-button variant="danger" @click="ok()"> Save </b-button>
+    </template>
+    <b-col>
+      <b-form-textarea
+        class="note-area"
+        placeholder="Enter Note"
+        v-model="note"
+      ></b-form-textarea>
+    </b-col>
+  </b-modal>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      note: "",
+    };
+  },
+  methods: {
+    // TODO
+    saveNote() {},
+  },
+};
+</script>
+
+<style>
+.note-area {
+  height: 200px !important;
+}
+</style>
--- client/src/components/Notes.vue
+++ client/src/components/Notes.vue
@@ -1,9 +1,21 @@
 <template>
-  <div>Notes</div>
+  <b-container fluid>
+    <b-col>
+      <b-row class="mt-3" align-h="end">
+        <b-button v-b-modal.createNoteModal>Create</b-button>
+        <CreateNoteDialog />
+      </b-row>
+    </b-col>
+  </b-container>
 </template>
 
 <script>
-export default {};
+import CreateNoteDialog from "./CreateNoteDialog";
+export default {
+  components: {
+    CreateNoteDialog,
+  },
+};
 </script>
 
 <style>
