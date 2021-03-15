618774ee6cc22de4ded607febbbc86bb2134344d
sevkioruc <sevkioruc34@gmail.com>
2021-03-16 01:06:54 +0300

    Note update way changes

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -49,30 +49,23 @@ module.exports = app => {
 		}
 	});
 
-	app.put('/notes/:id', verifyToken, async (req, res) => {
-		try {
-			const note = await Note.findOne({
-				'$and':
-					[
-						{ _id: req.params.id },
-						{ user: req.user.userId }
-					]
-			});
-			if (!note) {
-				res.status(401).json({ message: 'Access denied' });
-			} else {
-				note.title = req.body.title;
-				note.content = req.body.content;
-				note.save();
+	app.put('/notes/:id', verifyToken, (req, res) => {
+		const { note } = req.body;
+		const newUpdatedNote = new Note(note);
 
-				res.status(200).json({ message: 'Note updated succcessfully', note: note });
-			}
-		} catch (err) {
-			res.send({
-				message: 'Could not updated note'
-			});
-		}
-
+		Note.updateOne({ _id: req.params.id, user: req.user.userId }, newUpdatedNote)
+			.then(result => {
+				if (result.nModified > 0) {
+					res.status(200).json({
+						message: 'Update Successfully',
+					});
+				} else {
+					res.status(401).json({ message: 'Not authorized!' })
+				}
+			})
+			.catch(err => {
+				res.status(500).json({ err })
+			})
 	});
 
 };
