cbacb0362aeb5b2e57363b285ac1e40e6abf82c0
sevkioruc <sevkioruc34@gmail.com>
2021-03-20 02:24:43 +0300

    Verify token middleware updated

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -1,7 +1,8 @@
 const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
-	const token = req.header('Authorization').split(' ')[1];
+	const token = req.header('Authorization') ? req.header('Authorization').split(' ')[1] : null;
+
 	if (!token) {
 		return res.status(401).json({
 			error: 'Access denied'
