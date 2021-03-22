0707f7092b6b8f969e011ff4d65385a57850148d
sevkioruc <sevkioruc34@gmail.com>
2021-03-22 23:48:46 +0300

    Register page was re-designed

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,39 +1,47 @@
 <template>
-  <b-container fluid class="container">
-    <b-form class="register-form" @submit="onSubmit">
-      <b-form-group class="mb-3">
-        <b-form-input
-          v-model="form.email"
-          type="email"
-          placeholder="Email"
-          required
-        ></b-form-input>
-      </b-form-group>
+  <b-col class="container">
+    <b-row class="form-wrapper" align-h="center" align-v="center">
+      <b-form class="register-form" @submit="onSubmit">
+        <b-form-group class="mb-3">
+          <b-form-input
+            class="mb-1 mt-3"
+            v-model="form.email"
+            type="email"
+            placeholder="Email"
+            size="lg"
+            required
+          ></b-form-input>
+        </b-form-group>
 
-      <b-form-group class="mb-3">
-        <b-form-input
-          v-model="form.username"
-          placeholder="Username"
-          required
-        ></b-form-input>
-      </b-form-group>
+        <b-form-group>
+          <b-form-input
+            class="mb-2"
+            v-model="form.username"
+            placeholder="Username"
+            size="lg"
+            required
+          ></b-form-input>
+        </b-form-group>
 
-      <b-form-group>
-        <b-form-input
-          v-model="form.password"
-          type="password"
-          placeholder="Password"
-          required
-        ></b-form-input>
-      </b-form-group>
+        <b-form-group>
+          <b-form-input
+            class="mb-2"
+            v-model="form.password"
+            type="password"
+            placeholder="Password"
+            size="lg"
+            required
+          ></b-form-input>
+        </b-form-group>
 
-      <div class="d-flex">
-        <b-button class="ml-auto" type="submit" variant="primary">
-          Register
-        </b-button>
-      </div>
-    </b-form>
-  </b-container>
+        <div class="d-flex mb-2">
+          <b-button class="ml-auto" type="submit" size="lg" variant="primary">
+            Register
+          </b-button>
+        </div>
+      </b-form>
+    </b-row>
+  </b-col>
 </template>
 
 <script>
@@ -60,9 +68,16 @@ export default {
 
 <style scoped>
 .container {
-  text-align: center;
+  height: 100%;
+  background: linear-gradient(#e66465, #9198e5);
+}
+.form-wrapper {
+  height: 100%;
 }
 .register-form {
-  margin-top: 100px;
+  border: 1px solid lightgrey;
+  padding: 15px;
+  border-radius: 10px;
+  background-color: white;
 }
 </style>
