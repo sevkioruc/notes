cf14cf9abd82143e6b95f1e8c1049615586bc365
sevkioruc <sevkioruc34@gmail.com>
2021-03-28 03:15:05 +0300

    Homepage updated

--- client/src/components/Homepage.vue
+++ client/src/components/Homepage.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>Welcome !</div>
+  <b-col class="container mt-4 text-center">
+    <b-row class="description-wrapper" align-v="center" align-h="center">
+      <span class="description">Open Source Note Sharing Application</span>
+    </b-row>
+  </b-col>
 </template>
 
 <script>
@@ -7,4 +11,13 @@ export default {};
 </script>
 
 <style>
+.container {
+  height: 100%;
+}
+.description-wrapper {
+  height: 100%;
+}
+.description {
+  font-size: 35px;
+}
 </style>
